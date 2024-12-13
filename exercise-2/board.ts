5f10abd5b7d469af177e84825ae7fe61ffd49ab5
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-12-13 16:04:17 +0600

    Edited file board.ts, and also in file gitignore, added board.js so that it is ignored and not added to repository, because board.js is being automatically created from board.ts

--- exercise-2/board.ts
+++ exercise-2/board.ts
@@ -4,7 +4,7 @@ for (let i = 1; i <= 8; i++) {
     for (let j = 1; j <= 8; j++) {
 
         if (i % 2 === j % 2) {
-            blocksArray.push(' ');
+            blocksArray.push('');
         } else {
             blocksArray.push('■');
         }
